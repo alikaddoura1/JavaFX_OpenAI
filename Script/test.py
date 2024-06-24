84c52f9ec359225704a88e524902ef8e210b383c
alikaddoura1 <agakaddoura@gmail.com>
2024-06-24 15:26:30 +0300

    added python script

---
+++ Script/test.py
@@ -0,0 +1,15 @@
+# Author: Ali Kaddoura
+
+import sys
+
+def main():
+    # Read data from standard input
+    data = sys.stdin.read()
+    print("Data received:", data)
+
+if __name__ == "__main__":
+    main()
+
+
+
+
